79f89c323dd9d59bf61a48ca5e00bcf66c7cd1bc
Erik Masny <erik.masny@gmail.com>
2023-08-29 15:04:58 +0200

    feat: dont prefill by default, add button to prefill

--- resources/js/Pages/Posts/Create.vue
+++ resources/js/Pages/Posts/Create.vue
@@ -4,11 +4,18 @@ import AppTextarea from "@/components/AppTextarea.vue";
 import {useForm} from "@inertiajs/vue3";
 
 const post = useForm({
-    title: 'Moj novy clanok',
-    slug: 'moj-novy-clanok',
-    body: 'Obsah clanku',
-    published_at: '2023-08-01',
+  title: '',
+  slug: '',
+  body: '',
+  published_at: '',
 })
+
+const prefill = () => {
+  post.title = 'Moj novy clanok'
+  post.slug = 'moj-novy-clanok'
+  post.body = 'Obsah clanku'
+  post.published_at = '2023-08-01'
+}
 
 const storePost = async () => {
   post.clearErrors()
@@ -19,6 +26,9 @@ const storePost = async () => {
 
 <template>
     <form @submit.prevent="storePost" class="py-10">
+        <div class="text-right">
+            <button @click="prefill" type="button" class="text-sm bg-blue-600 px-4 py-1 rounded text-white">Prefill</button>
+        </div>
         <AppInput v-model="post.title" label="Title:" :error="post.errors.title" />
 
         <AppInput v-model="post.slug" label="Slug:" :error="post.errors.slug" />
